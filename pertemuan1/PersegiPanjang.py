a30e07b6047d2311c38bd30a216c533f9a17c54d
xjohanhalim <148331167+xjohanhalim@users.noreply.github.com>
2023-10-18 16:46:44 +0700

    Add files via upload

---
+++ pertemuan1/PersegiPanjang.py
@@ -0,0 +1,15 @@
+print ('Menghitung Luas dan Keliling Persegi Panjang')
+
+#Variabel
+panjang = 32
+lebar = 8
+
+#Rumus
+luas = panjang * lebar
+kel = (2 * panjang) + (2 * lebar)
+
+#Output
+print ("Panjang : ", panjang)
+print ("Lebar : ", lebar)
+print ("Luas : ", luas)
+print ("Keliling : ", kel)
